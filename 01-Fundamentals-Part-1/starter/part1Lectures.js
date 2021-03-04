e38bf300b8a0ca0593079425d0ddc2916397531d
Marius Pascui <mariuspascui@gmail.com>
2021-03-04 07:26:32 +0200

    Javascript basic & assignment operators
    Javascript basic & assignment operators.
    Started Lecture 1

---
+++ 01-Fundamentals-Part-1/starter/part1Lectures.js
@@ -0,0 +1,24 @@
+//=======VALUES & VARIABLES=======/
+
+/* let country = 'Romania';
+let continent = 'Europe';
+let population = '20 million';
+
+console.log(country, continent, population);
+
+//=======DATA TYPES=======/
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language); */
+
+//=======LET, CONST & VAR=======/
+
+const country = 'Romania';
+const continent = 'Europe';
+const language = 'Romanian';
+const isIsland = false;
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,5 @@
+//=======VALUES & VARIABLES=======/
+/*
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
@@ -6,3 +8,65 @@ console.log(23);
 
 let firstName = "Jonas";
 console.log(firstName);
+
+
+//=======DATA TYPES=======//
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Something");
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1992;
+console.log(typeof year);
+
+console.log(typeof null); 
+
+//=======LET, CONST & VAR=======//
+
+let age = 30;
+age = 31; 
+
+const birthYear = 1992;
+
+var job = 'programmer';
+job = 'teacher'; */
+
+//=======BASIC OPERATORS=======//
+
+// Math operators
+const now = 2037;
+const ageMarius = now - 1992;
+const ageSarah = now - 2018;
+console.log(ageMarius, ageSarah);
+
+console.log(ageMarius * 2, ageMarius / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3
+
+const firstName = 'Marius';
+const lastName = 'Pascui';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x + 1 = 101
+x--; // x - 1 = 100
+console.log(x);
+
+//Comparison operators (<, >, <=, >=)
+console.log(ageMarius > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
